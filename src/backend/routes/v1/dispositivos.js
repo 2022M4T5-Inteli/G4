5eeb645b66c6e7fe3138ef87fa4673ceaad44416
gabriosilva <gabrio.lina@hotmail.com>
2022-11-09 20:04:19 -0300

    feature(routes): implement dispositivo routes

--- src/backend/routes/v1/dispositivos.js
+++ src/backend/routes/v1/dispositivos.js
@@ -1,33 +1,94 @@
+// Required for accessing the database
 const { PrismaClient, Prisma } = require("@prisma/client");
-
 const prisma = new PrismaClient();
 
+// Required for route handling
 const express = require("express");
-
-// Router Controllers
-
 const router = express.Router();
 
-// controller
+// All Routes here (there should only be controllers) ----------------------------------------------------------------------------------------
 
-// GET requests
-router.get("/list", (request, response) => {
-  response.send({ message: "hello world! - list" });
+// [GET] requests
+
+router.get("/list", async (request, response) => {
+  // returns all the dispositivos that are in the database
+  const dispositivos = await prisma.dispositivo.findMany();
+  const responseObj = { data: dispositivos, error: false };
+  return response.json(responseObj);
 });
 
-// POST requests
+// [POST] requests
+
 router.post("/add", async (request, response) => {
-  const dispositivo = await prisma.dispositivo.create({
-    data: {
-      estufa: "estufa teste",
-      mac: "AABBCCDDEEFF",
+  const dispositivoMac = request.body.mac;
+  const dispositivoName = request.body.nome || dispositivoMac;
+  // checks if the current mac exists on the database
+  const dispositivoExists = await prisma.dispositivo.findUnique({
+    where: {
+      mac: dispositivoMac,
     },
   });
-  response.send({ message: "hello world! - insert" });
+
+  // Returns an error if the dispositivo already exists
+  if (dispositivoExists) {
+    response.statusCode = 500;
+    return response.json({
+      message: "O Dispositivo já existe",
+      data: {},
+      error: true,
+    });
+  }
+
+  // creates the dispositivo
+  const dispositivo = await prisma.dispositivo.create({
+    data: {
+      estufa: dispositivoName,
+      mac: dispositivoMac,
+    },
+  });
+
+  // returns the newly created dispositivo
+  response.statusCode = 200;
+  return response.send({
+    message: "Dispositivo Adicionado com sucesso!",
+    data: dispositivo,
+    error: false,
+  });
 });
 
-router.post("/edit", (request, response) => {
-  response.send({ message: "hello world! - edit" });
+router.post("/edit", async (request, response) => {
+  const dispositivoId = request.body.id;
+  const dispositivoName = request.body.nome;
+
+  // checks if the current mac exists on the database
+  const dispositivoExists = await prisma.dispositivo.findUnique({
+    where: {
+      id: dispositivoId,
+    },
+  });
+
+  // Returns an error if the dispositivo already exists
+  if (dispositivoExists) {
+    // updates the dispositivo by id
+    const updateDispositivo = await prisma.dispositivo.update({
+      where: {
+        id: dispositivoId,
+      },
+      data: {
+        estufa: dispositivoName,
+      },
+    });
+
+    response.statusCode = 200;
+    return response.json({
+      message: "Dispositivo editado com sucesso",
+      data: updateDispositivo,
+      error: false,
+    });
+  }
+
+  response.statusCode = 500;
+  return response.json({ message: "Erro ao editar dispositivo", error: true });
 });
 
 module.exports = router;
